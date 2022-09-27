148a12af2548a605174a62001bfd6a957a58c7d8
e-reds <109046469+e-reds@users.noreply.github.com>
2022-09-26 22:44:14 -0500

    update app.example .jsto app.js

--- src/admin/app.example.js
+++ src/admin/app.example.js
@@ -1,33 +1,22 @@
 const config = {
-  locales: [
-    // 'ar',
-    // 'fr',
-    // 'cs',
-    // 'de',
-    // 'dk',
-    // 'es',
-    // 'he',
-    // 'id',
-    // 'it',
-    // 'ja',
-    // 'ko',
-    // 'ms',
-    // 'nl',
-    // 'no',
-    // 'pl',
-    // 'pt-BR',
-    // 'pt',
-    // 'ru',
-    // 'sk',
-    // 'sv',
-    // 'th',
-    // 'tr',
-    // 'uk',
-    // 'vi',
-    // 'zh-Hans',
-    // 'zh',
-  ],
+  locales: ['es'],
+  translations: {
+    es: {
+      "app.components.HomePage.welcome": "Bienvenido a eLauncher!",
+      "app.components.HomePage.welcome.again": "Bienvenido",
+      "Auth.form.welcome.subtitle": "Inicie sesión en su cuenta de eLauncher",
+      "app.components.LeftMenu.navbrand.title": "Panel de control de eLauncher",
+      "app.components.HomePage.welcomeBlock.content.again": "Hola, espero que eLauncher te ayude en la gestion de tu biblioteca de juegos, si tienes problemas puedes pedir ayuda al WSP 51961283300",
+      "app.components.HomePage.community": "Encuentre ayuda con el desarrollador",
+      "app.components.HomePage.community.content": "Hable con el desarrollador de eLauncher, solicite una funcion, comente un problema, sugiera cambios, etc",
+      "global.documentation": "Apps",
+      "app.components.BlockLink.documentation.content": "Configura tus apps, muestralos, ocultalos, cambia de iconos",
+      "app.components.BlockLink.code": "Games",
+      "app.components.BlockLink.code.content":"Configura tus juegos, muestralos, ocultalos, cambia los cover, agrega videos, agrega imagenes, etc",
+    },
+  },
 };
+
 
 const bootstrap = (app) => {
   console.log(app);
