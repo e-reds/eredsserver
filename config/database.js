cfe3b8b8864fd5a5015cfce210ca3ffde801a54a
e-reds <109046469+e-reds@users.noreply.github.com>
2022-10-03 10:43:32 -0500

    postgress
    postgress

--- config/database.js
+++ config/database.js
@@ -1,11 +1,26 @@
 const path = require('path');
 
-module.exports = ({ env }) => ({
+/* module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     useNullAsDefault: true,
+  },
+});
+ */
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'ec2-52-0-142-65.compute-1.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'd7ro1l3sr98s3t'),
+      user: env('DATABASE_USERNAME', 'bmwocamarfwwoi'),
+      password: env('DATABASE_PASSWORD', '9d2413a2aff272dc057317053c6f8ce2ad31b7e0c1271dddf6023eebc2d345db'),
+      ssl: env.bool('DATABASE_SSL', true),
+    },
   },
 });
--- src/api/game/controllers/game.js
+++ src/api/game/controllers/game.js
@@ -6,4 +6,62 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::game.game');
+module.exports = createCoreController('api::game.game', {
+    async create(ctx) {
+        const user = ctx.state.user;
+
+        const game = await super.create(ctx);
+
+        const updated = await strapi.entityService.update("api::game.game", game.data.id, {
+            data: {
+                owner: user.id
+            }
+        });
+
+        return result;
+    },
+
+    async find(ctx){
+        const user = ctx.state.user;
+    
+        ctx.query.filters = {
+            ...(ctx.query.filters || {}),
+            owner: user.id
+        };
+    
+        return super.find(ctx);
+    },
+
+    async findOne(ctx){
+        const user = ctx.state.user;
+    
+        ctx.query.filters = {
+            ...(ctx.query.filters || {}),
+            owner: user.id
+        };
+    
+        return super.findOne(ctx);
+    },
+    async update(ctx){
+        const user = ctx.state.user;
+    
+        ctx.query.filters = {
+            ...(ctx.query.filters || {}),
+            owner: user.id
+        };
+    
+        return super.update(ctx);
+    },
+    async delete(ctx){
+        const user = ctx.state.user;
+    
+        ctx.query.filters = {
+            ...(ctx.query.filters || {}),
+            owner: user.id
+        };
+    
+        return super.delete(ctx);
+    }
+});
+
+
