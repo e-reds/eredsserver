88a7153fc51b3ccef6202e1ff8fb0cb3ef7b6f19
e-reds <109046469+e-reds@users.noreply.github.com>
2022-10-03 10:49:31 -0500

    postgress

--- config/database.js
+++ config/database.js
@@ -2,10 +2,14 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'ec2-52-0-142-65.compute-1.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'd7ro1l3sr98s3t'),
+      user: env('DATABASE_USERNAME', 'bmwocamarfwwoi'),
+      password: env('DATABASE_PASSWORD', '9d2413a2aff272dc057317053c6f8ce2ad31b7e0c1271dddf6023eebc2d345db'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
-    useNullAsDefault: true,
   },
 });
